ac2b0d6cb787a4bf87d970d30eaf71577c49432f
astrith katherine benavides imbachi <katherineimbachi044@gmail.com>
2026-08-19 12:31:32 -0500

    agrega el formulario de resgistro

---
+++ REGISTRO/registro.js
@@ -0,0 +1,26 @@
+let usuarios = [];
+
+function registrarUsuario() {
+
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    let codigo = document.getElementById("codigo").value;
+    let correo = document.getElementById("correo").value;
+    let contraseña = document.getElementById("contraseña").value;
+    let rol = document.getElementById("rol").value;
+
+    let usuario = {
+        nombre: nombre,
+        apellido: apellido,
+        codigo: codigo,
+        correo: correo,
+        contraseña: contraseña,
+        rol: rol
+    };
+
+    usuarios.push(usuario);
+
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+
+    alert("Usuario registrado correctamente");
+}
