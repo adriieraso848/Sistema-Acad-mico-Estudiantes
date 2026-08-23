f142d974c52a59c0ff9dac04b6706d2bf18d81a8
Adriana Erazo <monteroy848@gmail.com>
2026-08-23 17:07:42 -0500

    guardo cambios de panel

--- panel/panel.js
+++ panel/panel.js
@@ -1,37 +1,37 @@
-let usuario = JSON.parse(localStorage.getItem("usuario"));
+let usuario = JSON.parse(localStorage.getItem("usuarioActivo"));
 
 let bienvenido = document.getElementById("bienvenido");
 let contenido = document.getElementById("contenido");
 
 bienvenido.textContent = "Bienvenido " + usuario.nombre;
 
-if (usuario.rol == "administrador"){
+if (usuario.rol == "administrador") {
 
-    contenido.innerHTML = 
-    "<h2> Administrador </h2>" +
-    "<p> Gestión de estudiantes </p>" + 
-    "<p> Gestión de docentes </p>" + 
-    "<p> Reportes académicos </p>";
+    contenido.innerHTML =
+        "<h2>Administrador</h2>" +
+        "<p>Gestión de estudiantes</p>" +
+        "<p>Gestión de docentes</p>" +
+        "<p>Reportes académicos</p>";
 
-}else if (usuario.rol == "docente"){
+} else if (usuario.rol == "docente") {
 
-    contenido.innerHTML = 
-    "<h2> Docente </h2>" +
-    "<p> Cursos asignados </p>" + 
-    "<p> Registro de notas </p>" + 
-    "<p> Lista de estudiantes </p>";
+    contenido.innerHTML =
+        "<h2>Docente</h2>" +
+        "<p>Cursos asignados</p>" +
+        "<p>Registro de notas</p>" +
+        "<p>Lista de estudiantes</p>";
 
-}else if (usuario.rol == "estudiante"){
+} else if (usuario.rol == "estudiante") {
 
-    contenido.innerHTML = 
-    "<h2> Estudiante </h2>" +
-    "<p> Cursos matriculados </p>" + 
-    "<p> Notas </p>" + 
-    "<p> Información personal </p>";
+    contenido.innerHTML =
+        "<h2>Estudiante</h2>" +
+        "<p>Cursos matriculados</p>" +
+        "<p>Notas</p>" +
+        "<p>Información personal</p>";
 
-}else {
+} else {
 
-    contenido.innerHTML = 
-    "<h2> >Rol no reconocido </h2>" +
-    "<p> No se encontró un rol válido </p>";
+    contenido.innerHTML =
+        "<h2>Rol no reconocido</h2>" +
+        "<p>No se encontró un rol válido</p>";
 }
